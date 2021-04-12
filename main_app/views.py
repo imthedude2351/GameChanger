e84925f4dc903aed9609a87db1a20efb5cafb0be
khawarrr <mkhawar0907@gmail.com>
2021-04-12 15:32:25 -0700

    admin functionality

--- main_app/views.py
+++ main_app/views.py
@@ -3,10 +3,10 @@ from .models import Game
 
 # Create your views here.
 def home(request):
-  return render(request, 'about.html')
+  return render(request, 'home.html')
 
-# def about(request):
-#     return render(request, 'about.html')
+def about(request):
+    return render(request, 'about.html')
 
 def games_index(request):
     games = Game.objects.all()
