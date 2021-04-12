c6fb69b732636947b77615e8cf96c7dffef21372
Urian Muro <urianmuro@Urians-MacBook-Pro.local>
2021-04-12 11:45:04 -0700

    games page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    name = models.CharField(max_length=150)
+    developer = models.CharField(max_length=200)
+    rating = models.CharField(max_length=5)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+# class Game: 
+#     def __init__(self, name, developer, rating, year):
+#         self.name = name
+#         self.developer = developer
+#         self.rating = rating
+#         self.year = year
+
+# games = [
+#     Game('The Last of Us 2', 'Naughty Dog', 'M', 2020),
+#     Game('Call of Duty: Black Ops', 'TreyArch', 'M', 2010),
+#     Game('StarCraft 2', 'Blizzard', 'T', 2010),
+#     Game('Ghost of Tsushima', 'Sucker Punch Production', 'M', 2020)
+# ]
