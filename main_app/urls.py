0fa8055ae78ced6e0be26c49486d76f6fa584062
Erwin Medina <erwin.l.medina@gmail.com>
2021-04-12 21:39:40 -0700

    fixed CSS

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
-
-
-
 from . import views
 
 urlpatterns = [
@@ -12,5 +9,5 @@ urlpatterns = [
     path('games/<int:game_id>/', views.games_detail, name='detail'),
     path('games/create/', views.GameCreate.as_view(), name='games_create'),
     path('games/<int:pk>/update/', views.GameUpdate.as_view(), name='games_update'),
-  path('games/<int:pk>/delete/', views.GameDelete.as_view(), name='games_delete'),
+    path('games/<int:pk>/delete/', views.GameDelete.as_view(), name='games_delete'),
 ]
